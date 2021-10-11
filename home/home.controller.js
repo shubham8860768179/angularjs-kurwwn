7344d6131a1221f14a0180f08e5162c665ec1799
shubham8860768179 <shubham.yadav001@hotmail.com>
2021-10-11 01:23:43 -0700

    2 files modified

--- home/home.controller.js
+++ home/home.controller.js
@@ -2,7 +2,15 @@ class HomeCtrl {
   constructor($scope) {
     'ngInject';
 
-    this.name = "AngularJS";
+    this.name = 'AngularJS';
+
+    this.genders = [];
+    this.genders.push('Male');
+    this.genders.push('Female');
+  }
+
+  onSubmit(){
+    console.log("on submit");
   }
 }
 
